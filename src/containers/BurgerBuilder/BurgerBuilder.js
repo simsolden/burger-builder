ef8ff728047989406a9370463cdda9a246a01075
Simon Oldenhove <simonoldenhove@gmail.com>
2020-11-17 21:36:12 +0100

    adding ingredients dynamically: beginning

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,19 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      cheese: 2,
+      bacon: 3,
+      meat: 0,
+    },
+  };
+
   render() {
     return (
       <Fragment>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Fragment>
     );
