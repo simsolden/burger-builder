0c839d28af0bc20e2c09b9a3579ad403fee339cc
Simon Oldenhove <simonoldenhove@gmail.com>
2020-11-17 15:58:10 +0100

    first hardcoded burger

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import Layout from './components/Layout/Layout';
 import './App.css';
 import React, { Component } from 'react';
+
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
