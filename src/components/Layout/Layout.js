c42f77e9de04170c80371517ba729aabafdc57a2
Simon Oldenhove <simonoldenhove@gmail.com>
2020-11-19 18:11:43 +0100

    logo and sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,32 @@
-import React, { Fragment } from 'react';
-//import Aux from '../../hoc/Auxiliary';
+import React, { Component, Fragment } from 'react';
+
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Fragment>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </Fragment>
-);
+class Layout extends Component {
+  state = { showSideDrawer: false };
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Toolbar />
+        <SideDrawer
+          show={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Fragment>
+    );
+  }
+}
+
+export default Layout;
